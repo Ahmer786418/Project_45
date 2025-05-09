af7a88f3671130c87837262a5c5be6969c504cad
Ahmer786418 <ahmerabbas400@gmail.com>
2025-05-09 10:30:41 +0500

    Add files via upload

---
+++ task 10 (adding comments)/app.ts
@@ -0,0 +1,32 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at 
+least one comment to each. If you don’t have anything specific to write
+ because your programs are too simple at this point, just add your name and
+  the current date at the top of each program file. Then write one sentence
+   describing what the program does.
+*/
+
+
+
+// store my favorite number in a variable
+const favorite_number : number = 99;
+//revealing my favorite number
+const messege : string = (`My Favorite number is ${favorite_number}`);
+console.log(messege) ;
+
+
+// storing friends name in array
+let names : string[] = ["Affan","Ali","King","Ahmer"];
+
+//printing names one by one
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+
+
+
+
+
+
+
