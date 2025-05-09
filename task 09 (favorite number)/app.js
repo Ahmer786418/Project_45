5f4e6bf2e2f16457535c07ff13aa2f1ca48a211e
Ahmer786418 <ahmerabbas400@gmail.com>
2025-05-09 10:28:23 +0500

    Add files via upload

---
+++ task 09 (favorite number)/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message
+that reveals your favorite number. Print that message
+*/
+const favorite_number = 99;
+const messege = (`My Favorite number is ${favorite_number}`);
+console.log(messege);
