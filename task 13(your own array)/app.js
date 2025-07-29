3cda3d86c9b04097c8f425a7188578d880fef876
Ahmer786418 <ahmerabbas400@gmail.com>
2025-07-29 14:55:29 +0500

    Add files via upload

---
+++ task 13(your own array)/app.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a
+list that stores several examples. Use your list to print a series of statements about these items, such as
+“I would like to own a Honda motorcycle.”
+
+*/
+let transportation = ["Car", "Motorcycle", "heavy bike", "aeroplane"];
+console.log(`"I would like to own a honda": ${transportation[0]}`);
+console.log(`"I would like to own a honda": ${transportation[1]}`);
+console.log(`"I would like to own a honda": ${transportation[2]}`);
+console.log(`"I would like to own a honda": ${transportation[3]}`);
---
+++ task 13(your own array)/app.ts
@@ -0,0 +1,15 @@
+/*
+Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a 
+list that stores several examples. Use your list to print a series of statements about these items, such as 
+“I would like to own a Honda motorcycle.”
+
+*/
+
+let transportation : string[] = ["Car","Motorcycle","heavy bike","aeroplane"];
+
+
+console.log(`"I would like to own a honda": ${transportation[0]}`);
+console.log(`"I would like to own a honda": ${transportation[1]}`);
+console.log(`"I would like to own a honda": ${transportation[2]}`);
+ console.log(`"I would like to own a honda": ${transportation[3]}`);
+
