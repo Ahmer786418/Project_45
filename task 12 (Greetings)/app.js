4c5e43ee70331dc723475f3e749c7b7afd7ba526
Ahmer786418 <ahmerabbas400@gmail.com>
2025-07-29 14:52:45 +0500

    Add files via upload

---
+++ task 12 (Greetings)/app.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a
+message to them. The text of each message should be the same, but each message should be personalized with the
+person’s name.
+*/
+let names = ["Affan", "Ali", "King", "Ahmer"];
+console.log(`make sure to suscribe my channel,${names[0]}`);
+console.log(`make sure to suscribe my channel,${names[1]}`);
+console.log(`make sure to suscribe my channel,${names[2]}`);
+console.log(`make sure to suscribe my channel,${names[3]}`);
---
+++ task 12 (Greetings)/app.ts
@@ -0,0 +1,16 @@
+/*
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a 
+message to them. The text of each message should be the same, but each message should be personalized with the 
+person’s name.
+*/
+
+
+
+
+let names : string[] = ["Affan","Ali","King","Ahmer"];
+
+
+console.log(`make sure to suscribe my channel,${names[0]}`);
+console.log(`make sure to suscribe my channel,${names[1]}`);
+console.log(`make sure to suscribe my channel,${names[2]}`);
+console.log(`make sure to suscribe my channel,${names[3]}`);
