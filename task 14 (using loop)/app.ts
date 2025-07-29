878ac79b74e75c866c9d4284d6e3c47f9be3e59e
Ahmer786418 <ahmerabbas400@gmail.com>
2025-07-29 15:29:15 +0500

    Add files via upload

---
+++ task 14 (using loop)/app.ts
@@ -0,0 +1,19 @@
+/*Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that 
+includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person,
+ inviting them to dinner.
+
+*/
+
+
+
+
+let Guest_list : string[] = ["Ahmer","Ubaid","Ali"];
+let i= 0;
+while(i <  Guest_list.length){
+
+        console.log(`Dear ${Guest_list[i]}:  \n you are invited to dinner! \n `)
+
+i++
+}
+
+
