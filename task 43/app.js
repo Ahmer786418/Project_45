bbbe7714ed2e7188599e2269584dd359af8d8ea8
Ahmer786418 <ahmerabbas400@gmail.com>
2025-08-02 17:23:07 +0500

    Add files via upload

---
+++ task 43/app.js
@@ -0,0 +1,29 @@
+"use strict";
+//EXERCISE 43
+/*
+Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the
+array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a
+separate array. Call show_magicians() with each array to show that you have one array of the original names and
+one array with the Great added to each magician’s name.
+
+*/
+let magicians = ["Alice", "David", "Chris"];
+function show_magicians(magician) {
+    let copyMagician = [...magician];
+    // console.log("Magicians: ", magicians)
+    // make_great()
+    console.log("Magician: ", magician);
+    return copyMagician;
+}
+function make_great(magicians) {
+    let greatMagicians = [...magicians];
+    magicians.forEach(magician => {
+        greatMagicians.push(`${magician} the Great `);
+    });
+    return greatMagicians;
+}
+let greatMagicians = make_great(magicians.slice());
+console.log("Orinal magicians:");
+show_magicians(magicians); // shows orignal magicians
+console.log("great magicians");
+show_magicians(greatMagicians); //show modified names
