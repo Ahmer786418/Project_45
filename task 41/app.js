7ccfadaac9c896d9f9b051f8acdbe622cd252f73
Ahmer786418 <ahmerabbas400@gmail.com>
2025-08-02 17:20:07 +0500

    Add files via upload

---
+++ task 41/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// exercise 41
+let magicians = ["Ayaz", "Fayaz", "Tariq", "Madad"];
+function show_magicians(magicians) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ task 41/app.ts
@@ -0,0 +1,14 @@
+// exercise 41
+
+
+let magicians: string[] = ["Ayaz", "Fayaz", "Tariq", "Madad"];
+
+
+function show_magicians(magicians:string[]){
+    magicians.forEach(magician => {
+        console.log(magician);
+        
+    });
+}
+
+show_magicians(magicians)
