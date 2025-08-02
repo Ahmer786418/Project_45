149a6ff5c2d891b1536d8d2d8b0308c095257e09
Ahmer786418 <ahmerabbas400@gmail.com>
2025-08-02 16:27:53 +0500

    Add files via upload

---
+++ task 21/app.js
@@ -0,0 +1,13 @@
+"use strict";
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects containing
+these items.
+*/
+let object = {
+    Name: "ahmer",
+    age: 20,
+    profession: "student"
+};
+console.log(`\n object containing information about me : \n`);
+console.log(`My name is ${object.Name}`);
+console.log(`my age is ${object.age}`);
+console.log(`i am a ${object.profession}`);
