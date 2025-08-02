3d23800965b71461fdeddcbf6c73de35b8d65a90
Ahmer786418 <ahmerabbas400@gmail.com>
2025-08-02 16:29:27 +0500

    Add files via upload

---
+++ task 22/app.ts
@@ -0,0 +1,15 @@
+/*
+Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen.
+ Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the
+  program.
+
+
+*/
+
+let Arr = ['Ahmer',19,'student'];
+console.log("let showing error");
+console.log(Arr[3] +'\n');
+console.log("correction of error");
+console.log(Arr[2]);
+
+
