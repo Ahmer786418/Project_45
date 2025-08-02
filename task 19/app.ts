f29fca6a3ed54e0a6819ed2e23a224194872bbba
Ahmer786418 <ahmerabbas400@gmail.com>
2025-08-02 16:24:34 +0500

    Add files via upload

---
+++ task 19/app.ts
@@ -0,0 +1,7 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the 
+// number of people you are inviting to dinner.
+
+let Guest_List :string[] =["Akram","Ahmer","zubair"];
+
+console.log(`Total no of people invited ${Guest_List.length}`);
+
